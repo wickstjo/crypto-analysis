b4dd889c5a016df5c32c277aa36b1f5dcc32d985
wickstjo <33825194+wickstjo@users.noreply.github.com>
2018-10-09 08:04:57 +0300

    Started with boxplot

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // SET QUERY PROPS
 var data = {
    from: 'ETH',
-   to: 'BTC',
-   days: 100
+   to: 'EUR',
+   days: 50
 }
 
 // STITCH QUERY STRING TOGETHER
@@ -12,20 +12,39 @@ var query = 'https://min-api.cryptocompare.com/data/histoday?fsym=' + data.from 
 d3.json(query).then((response) => {
 
    // SET CHILD CONTAINERS
-   data.daily = { sold: [], exchange: [0] };
-   data.ranges = [];
+   data.daily = {
+      sold: [],
+      exchange: [0] // SET ZERO AS STARTING COORDINATE FOR LINE CHART
+   };
+
+   data.ranges = {
+      highest: [],
+      lowest: [],
+      lengths: []
+   };
 
    // SET RELEVANT DATA TO MAIN OBJECT
    for (var x = 0; x < response.Data.length; x++) {
       data.daily.sold.push(response.Data[x].volumefrom);
       data.daily.exchange.push(response.Data[x].volumeto);
-      data.ranges.push({
-         high: response.Data[x].high,
-         low: response.Data[x].low,
-         avg: (response.Data[x].low + response.Data[x].high) / 2
-      });
+
+      data.ranges.highest.push(response.Data[x].high);
+      data.ranges.lowest.push(response.Data[x].low);
+      data.ranges.lengths.push(response.Data[x].high - response.Data[x].low);
    }
 
+   // REMOVE LAST ELEMENT, IE TODAYS DATA, FOR READABILITY
+   data.daily.exchange.pop()
+   data.daily.sold.pop()
+   data.ranges.highest.pop()
+   data.ranges.lowest.pop()
+   data.ranges.lengths.pop()
+
+   // FIND & SET HIGHEST AND LOWEST VALUES IN RANGES FOR SCALING
+   data.ranges.lowest = d3.min(data.ranges.lowest);
+   data.ranges.highest = d3.max(data.ranges.highest);
+
+   // PUSH IN ENDING COORDINATE FOR LINE CHART
    data.daily.exchange.push(0)
 
    // APPEND IN BOXES FOR THE GRAPHS
@@ -70,7 +89,7 @@ d3.json(query).then((response) => {
    data.path = pathify(data.daily.exchange);
    log(data)
 
-   // LINE CHART STARTS
+                                                                                 // LINE CHART STARTS
 
    // GENERATE GRAPH CANVAS
    var canvas = d3.select('#linechart').append('svg')
@@ -87,8 +106,8 @@ d3.json(query).then((response) => {
       .attr('opacity', settings.opacity)
       .attr('d', data.path)
 
-   // LINE CHART ENDS
-   // BAR CHART STARTS
+                                                                                 // LINE CHART ENDS
+                                                                                 // BAR CHART STARTS
 
    // ADD LINEAR SCALING
    var yScale = d3.scaleLinear()
@@ -123,11 +142,41 @@ d3.json(query).then((response) => {
          .attr('stroke-width', settings.border.size)
          .attr('opacity', settings.opacity)
 
-      // BAR CHART ENDS
-      // BOX PLOT STARTS
+                                                                                 // BAR CHART ENDS
+                                                                                 // BOX PLOT STARTS
 
+      // ADD LINEAR SCALING
+      var yScale = d3.scaleLinear()
+         .domain([0, d3.max(data.ranges.lengths)])
+         .range([0, settings.height])
+
+      // ADD ORDINAL SCALING
+      var xScale = d3.scaleBand()
+         .domain(data.ranges.lengths)
+         .range([0, settings.width])
+         .paddingInner(settings.padding / 100)
+
+      // GENERATE CANVAS
       var canvas = d3.select('#boxplot').append('svg')
+
+         // ADD CUSTOMIZED PROPERTIES
+         .attr('width', settings.width)
          .attr('height', settings.height)
+         .style('background', settings.background)
+
+         // GENERATE 'BARS' BY LOOPING THROUGH DATA
+         canvas.selectAll('bars').data(data.ranges.lengths).enter()
+
+            // ADD CUSTOMIZED PROPERTIES
+            .append('rect')
+            .attr('width', xScale.bandwidth())
+            .attr('height', (d) => { return yScale(d); })
+            .attr('x', (d) => { return xScale(d); })
+            .attr('y', (d) => { return settings.height - yScale(d); })
+            .attr('fill', settings.background.green)
+            .attr('stroke', settings.border.color)
+            .attr('stroke-width', settings.border.size)
+            .attr('opacity', settings.opacity)
 
       // BOX PLOT ENDS
 });
